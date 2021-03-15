3ba27cff66a05ff946f099d0595f02d64e2c4d2e
Chris Harvey <chris@chrisnharvey.com>
2021-03-15 12:20:32 +0000

    Add primary key state class

---
+++ src/RequirePrimaryKeyState.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ChrisHarvey\LaravelSqlRequirePrimaryKey;
+
+use Illuminate\Support\Facades\DB;
+
+class RequirePrimaryKeyState
+{
+    protected static array $originals = [];
+
+    public static function set(bool $status, string $connection = null): void
+    {
+        if (! static::hasOriginal($connection)) {
+            static::saveOriginal();
+        }
+
+        DB::statement('/*!80013 SET SQL_REQUIRE_PRIMARY_KEY = ?;*/', [$status]);
+    }
+
+    public static function get(string $connection = null): bool
+    {
+        $requiresPrimaryKey = DB::connection($connection)
+            ->selectOne('SHOW SESSION VARIABLES LIKE "sql_require_primary_key";')->Value ?? 'OFF';
+
+        return $requiresPrimaryKey == 'ON';
+    }
+
+    public static function hasOriginal(string $connection = null): bool
+    {
+        return isset(static::$originals[$connection]);
+    }
+
+    public static function getOriginal(string $connection = null): bool
+    {
+        return static::$originals[$connection];
+    }
+
+    public static function saveOriginal(string $connection = null): void
+    {
+        static::$originals[$connection] = static::get($connection);
+    }
+
+    public static function restore(string $connection = null): void
+    {
+        static::set(static::$originals[$connection], $connection);
+    }
+}
